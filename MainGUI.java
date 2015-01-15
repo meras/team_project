f77b507fcbea398a2ebcf6a6bf28ba8b99f1edd0
meras <allmost@gmail.com>
2015-01-15 16:42:56 +0000

    added a missing comma and an empty actionPeformed method (IDE did let me compile work)

--- MainGUI.java
+++ MainGUI.java
@@ -11,12 +11,14 @@ public class MainGUI extends JFrame implements ActionListener
 {
 	private JPanel left, center, right;
 	private JLabel matchTitle, addRefTitle, searchRefTitle, 
-		weekLabel, locationLabel, levelLabel, findRefLabel, firstNameLabel
+		weekLabel, locationLabel, levelLabel, findRefLabel, firstNameLabel,
 		lastNameLabel, addRefLabel;
 	private JTextField weekField, firstNameField, lastNameField;
 	private JButton findRefButton, searchRefButton, addRefButton;
 	private JRadioButton north, central, south, junior, senior;
-	
-	
+
+	public void actionPerformed(ActionEvent e) {
+		//
+	}
 	
 }
