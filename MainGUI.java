39da6f0008fda790dc4d22ca8c0be753c08c481e
Mary McDowall <msmcdowall@gmail.com>
2015-01-15 14:58:32 +0000

    Main gui instance variables added

---
+++ MainGUI.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+
+/**
+* Main GUI 
+*/ 
+public class MainGUI extends JFrame implements ActionListener
+{
+	private JPanel left, center, right;
+	private JLabel matchTitle, addRefTitle, searchRefTitle, 
+		weekLabel, locationLabel, levelLabel, findRefLabel, firstNameLabel
+		lastNameLabel, addRefLabel;
+	private JTextField weekField, firstNameField, lastNameField;
+	private JButton findRefButton, searchRefButton, addRefButton;
+	private JRadioButton north, central, south, junior, senior;
+	
+	
+	
+}
