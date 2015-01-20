91f1292ba69277388f8f48fbda358750ac5b38b3
Mary McDowall <msmcdowall@gmail.com>
2015-01-20 16:36:16 +0000

    level buttons and center panel started

--- MainGUI.java
+++ MainGUI.java
@@ -17,6 +17,8 @@ public class MainGUI extends JFrame implements ActionListener
 	private JButton findRefButton, searchRefButton, addRefButton;
 	private JRadioButton northButton, centralButton, southButton, juniorButton, seniorButton;
 	private ButtonGroup locationGroup, levelGroup;
+    private JScrollPane centerScroll;
+
 	public MainGUI()
 	{
 		this.setTitle("Referee Selection"); //Provisional title
@@ -31,13 +33,17 @@ public class MainGUI extends JFrame implements ActionListener
 		center = new JPanel();
 		right = new JPanel();
 		
-		// Create label and textField for week number
+        // Create label for title/instructions for left panel
+      
+
+
+		// Create label and textField for match week number
 		weekLabel = new JLabel("Week Number (1-52)");
 		left.add(weekLabel);
 		weekField = new JTextField(2);
 		left.add(weekField);
 		
-		//Create Label and radio buttons for location
+		//Create Label and radio buttons for match location
 		locationLabel = new JLabel("Match Location");
 		left.add(locationLabel);
 		
@@ -50,13 +56,38 @@ public class MainGUI extends JFrame implements ActionListener
 		southButton = new JRadioButton("South");
 		//Code for when South Location is selected
 		
-		//Group the JRadioButtons so that they are mutually exclusive
+		//Group the location JRadioButtons so that they are mutually exclusive
 		locationGroup = new ButtonGroup();
 		locationGroup.add(northButton);
 		locationGroup.add(centralButton);
 		locationGroup.add(southButton);
+        left.add(locationGroup);
 		//This will need to be altered so that they display horizontally
-		
+
+        // Create label and radio buttons for level        
+        levelLabel = new JLabel("Level");
+        left.add(levelLabel);
+        
+        juniorButton = new JRadioButton("Junior");  
+        // Code for when junior level is selected
+
+        seniorButton = new JRadioButton("Senior");
+        // Code for when senior is seleted
+
+        // Group the level JRadioButtons so they are mutually exclusive
+        levelGroup = new ButtonGroup();
+        levelGroup.add(juniorButton);
+        levelGroup.add(seniorButton);
+        left.add(levelGroup);
+
+        //Create JTable to display list of referees
+        
+        //Create JScrollPane and add to center panel
+        centerScroll = new JScrollPane();
+        // Add JTable component to scroll pane
+        center.add(centerScroll);
+
+        
 	}
 	
 	public void actionPerformed(ActionEvent e) {
