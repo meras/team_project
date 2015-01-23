47e98279daaa34b04022a3d56f9cb9355d671eeb
meras <allmost@gmail.com>
2015-01-23 13:06:10 +0000

    edit button works

--- LittleGUI.java
+++ LittleGUI.java
@@ -9,6 +9,15 @@ import java.awt.event.ActionListener;
 public class LittleGUI extends JFrame implements ActionListener {
     private JButton editButton, saveButton, deleteButton, clearButton, cancelButton;
     private final String[] qualificationList = {"", "First", "Second", "Third"};
+    private JTextField fNameField;
+    private JTextField lNameField;
+    private JTextField idField;
+    private JTextField matchField;
+    private JComboBox qualificationsCombo;
+    private JRadioButton northRadio;
+    private JRadioButton centerRadio;
+    private JRadioButton southRadio;
+    private JPanel bottom;
 
     ///ADD ref - everything is editable
     //SEARCH-
@@ -16,7 +25,7 @@ public class LittleGUI extends JFrame implements ActionListener {
     public LittleGUI(String mode) {
         setTitle("edit info");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setSize(350, 300);
+        setSize(350, 400);
         setLocationRelativeTo(null);
 
         layoutCenter();
@@ -27,17 +36,16 @@ public class LittleGUI extends JFrame implements ActionListener {
      * center grid to house other grids
      */
     private void layoutCenter() {
-        JPanel center = new JPanel(new GridLayout(5,1,2,2));
+        JPanel center = new JPanel(new GridLayout(6,1,2,2));
 
-        JTextField fNameField = new JTextField(15);
-        JTextField lNameField = new JTextField(15);
-        JTextField idField = new JTextField(6);
+        fNameField = new JTextField(15);
+        lNameField = new JTextField(15);
+        idField = new JTextField(6);
         idField.setEditable(false);
-        JTextField matchField = new JTextField(7);
+        matchField = new JTextField(7);
         matchField.setEditable(false);
-        JComboBox qualificationsCombo = new JComboBox(qualificationList);
+        qualificationsCombo = new JComboBox(qualificationList);
 
-        JRadioButton northRadio, centerRadio, southRadio;
         ButtonGroup homeGroup = new ButtonGroup();
 
         JPanel fname = new JPanel();
@@ -70,18 +78,31 @@ public class LittleGUI extends JFrame implements ActionListener {
         home.add(centerRadio);
         home.add(southRadio);
 
-        //add all the components to the center JPanel
+        JPanel preferrence = new JPanel();
+        northRadio = new JRadioButton("North");
+        centerRadio = new JRadioButton("Center");
+        southRadio = new JRadioButton("South");
+        homeGroup.add(northRadio);
+        homeGroup.add(centerRadio);
+        homeGroup.add(southRadio);
+        preferrence.add(new JLabel("Home:"));
+        preferrence.add(northRadio);
+        preferrence.add(centerRadio);
+        preferrence.add(southRadio);
+
+        //add all the panels to the center JPanel
         center.add(fname);
         center.add(lname);
         center.add(info);
         center.add(qualification);
         center.add(home);
+        center.add(preferrence);
         add(center, BorderLayout.CENTER);
 
     }
 
     private void layoutBottom(String mode) {
-        JPanel bottom = new JPanel();
+            bottom = new JPanel();
 
         clearButton = new JButton("Clear");
         editButton = new JButton("Edit");
@@ -95,20 +116,37 @@ public class LittleGUI extends JFrame implements ActionListener {
         saveButton.addActionListener(this);
         deleteButton.addActionListener(this);
 
-        if (mode == "Add") {
+        if (mode.equals("Add")) {
+            bottom.add(saveButton);
+            bottom.add(cancelButton);
+            //bottom.add(clearButton);
+        }
+        else if (mode.equals("Search")){
+            bottom.add(editButton);
+            bottom.add(deleteButton);
+            bottom.add(cancelButton);
+        }
+        else if (mode.equals("Edit")) {
             bottom.add(saveButton);
             bottom.add(cancelButton);
         }
-        else {
-            bottom.add(clearButton);
-            bottom.add(editButton);
-            bottom.add(saveButton);
-            bottom.add(deleteButton);
-        }
+
         add(bottom, BorderLayout.SOUTH);
     }
 
+    private void showEdit() {
+        for (Component c : bottom.getComponents()) {
+            c.setVisible(false);
+        }
+        editButton.setVisible(true);
+    }
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == deleteButton) {
+            System.out.println("Are you sure you want to delete this entry");
+        }
+        if (e.getSource() == editButton) {
+            showEdit();
+        }
 
     }
 }
