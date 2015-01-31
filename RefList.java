361f7165ca02858f40427b0bf3281179fb46c75e
tvz <tvzeller@gmail.com>
2015-01-31 21:46:14 +0000

    commented RefList, slight refactoring of getSuitableRefs()

--- RefList.java
+++ RefList.java
@@ -9,97 +9,113 @@ public class RefList implements Iterable<Referee> {
 		refList = new ArrayList<Referee>();
 	}
 
-	/*
-	 * to be used in GUI when checking if a new ref can be added
+	/**
+	 * Checks if there is still room for another Referee, returns true if there is.
+	 * @return boolean indicating whether there is room
 	 */
 	public boolean checkForSpace() {
 		return (refList.size() < MAX_REFS);
 	}
 
-	/*
-	 * as the refs are in order as we read them from the file, here we can just add
-	 * the ref to the end of the arraylist
+	/**
+	 * Creates a Referee from info read from input file and adds it to the end of refList.
+	 * @param refString the info line read in from the file 
 	 */
 	public void addRefFromFile(String refString) {
 		Referee newRef = new Referee(refString);
 		refList.add(newRef);
 	}
 
+	/**
+	 * Gets the current amount of Referees in refList
+	 * @return an int corresponding to the size of refList
+	 */
 	public int getRefereeCount() {
 		return refList.size();
 	}
 
-	/*
-	 * when a new ref is added from the GUI, we first call a method to create an ID based on their name, pass the
-	 * info to the Referee constructor and then loop through the arraylist to find where the
-	 * ref should be inserted. Done it this way for now but there may be better ways, for example
-	 * Merunas you had an idea for this right?
-	 * N.B. to create a new Referee I am using the same constructor as used when creating from the file,
-	 * i.e. I am making a string from all the info and passing it to the constructor.
-	 * This isn't ideal but seems nicer than having a constructor with 7 parameters.
-	 * To be discussed...
+	/**
+	 * Used to add a Refere object fom information input to the GUI. First calls method to create the ref's ID then
+	 * creates a new Referee, checks where it should be placed in refList based on order of IDs and adds it 
+	 * to refList in that position.
+	 * @param firstNm the first name input to the GUI
+	 * @param lastNm the last name input to the GUI
+	 * @param qual the ref's qualification
+	 * @param allocs the ref's number of match allocations
+	 * @param home the ref's home area
+	 * @param travelInfo information on where the ref will travel
 	 */
 	public void addRefFromGui(String firstNm, String lastNm, String qual, int allocs, String home, String travelInfo) 
 	{
 		String newId = createId(firstNm, lastNm);
+		//create string to pass to Referee constructor
 		String refData = newId + " " + firstNm + " " + lastNm + " " + qual + " " + allocs + " " + home + " " + travelInfo;
 		Referee newRef = new Referee(refData);
 
+		//place new Referee in refList based on ID order
 		boolean added = false;
 		int i = 0;
-		while(!added && i < refList.size()) {
-			String otherID = refList.get(i).getRefID();
+		while(!added && i < refList.size()) { //loop through refList
+			String otherID = refList.get(i).getRefID(); //get ID to compare 
+			//if new ref's ID should be before compared ID, add new ref at compared ref's position
 			if(newId.compareTo(otherID) < 0) {
 				refList.add(i, newRef);
-				added = true;
+				added = true; //new ref has been added, exit loop
 			}
 			else
-				i++;
+				i++; //if new ref should be placed after compared ref, check next position
 		}
-
+		//If new ID does not come before any existing ID, place new Ref at end of refList
 		if(!added)
 			refList.add(newRef);
 	}
 
+	/**
+	 * generates a unique ID for a ref added from the GUI based on first and last name
+	 * @param first the new ref's first name
+	 * @param last the new ref's last name
+	 * @return the new ref's ID
+	 */
 	private String createId(String first, String last) {
+		//get letter part of ID from initials
 		String letterPart = "" + first.charAt(0) + last.charAt(0);
-		int numPart = 1;
+		int numPart = 1; // set number part to 1 initially
 
-		for(int i = 0; i < refList.size(); i++) {
+		for(int i = 0; i < refList.size(); i++) { //loop through refList
 			Referee ref = refList.get(i);
-			String otherInitials = ref.getRefID().substring(0,2);
+			String otherInitials = ref.getRefID().substring(0,2); //get compared ref's initials
 			if(letterPart.equals(otherInitials))
-				numPart++;
+				numPart++; //if matching initials found, increment the number part of ID
 		}
-
+		//concatenate the letter part and number part
 		String newRefId = letterPart.toUpperCase() + numPart;
 		return newRefId;
 	}
 
-	/*
-	 * What I am imagining with the 2 methods below: the GUI reads in the ref's
-	 * first and last name. It passes them to findFromName, which returns the index
-	 * position of the ref within the list (or a "sentinel value" if the ref was not found).
-	 * Then the GUI class calls deleteRef using that index.
+	/**
+	 * used to delete a Referee from refList
+	 * @param first the ref's first name
+	 * @param last the ref's last name
+	 * @return true if the ref of that name was found and deleted, false if not found
 	 */
 
 	public boolean deleteRef(String first, String last)
 	{
-		Referee findRefResult = findRef(first, last);
+		Referee findRefResult = findRef(first, last); //look for ref in refList
 
-		if (findRefResult != null)
+		if (findRefResult != null) //if ref exists, remove from refList
 		{
 			refList.remove(findRefResult);
 			return true;
 		}
-		else
+		else //ref not found, return false
 			return false;
 	}
 
 	/**
-	 *
-	 * I liked Kalin's idea to just have a method to return a ref from search instead of an index.
-	 * This is just a coffee of the above for now as I didn't want to change Tomasz's code (which is beautiful)
+	 * looks for a Referee in refList based on their name
+	 * @param first the ref's first name
+	 * @param last the ref's last name
 	 * @return Referee object if found, else null
 	 */
 	public Referee findRef(String first, String last) {
@@ -107,36 +123,22 @@ public class RefList implements Iterable<Referee> {
 		boolean found = false;
 		int refIndex = 0;
 
-		while(!found && refIndex < refList.size()) {
+		while(!found && refIndex < refList.size()) { //loop through refList
 			ref = refList.get(refIndex);
+			//compare name to name of ref at position i
 			if(ref.getFName().equals(first) && ref.getLName().equals(last))
-				found = true;
+				found = true; //if found, exit loop
 			else
-				refIndex++;
+				refIndex++; //if not found check against next ref
 		}
 
-		if(!found)
+		if(!found) //if ref not in refList return null
 			return null;
-		else
+		else //if ref was found, return the object
 			return ref;
 	}
 
-	/*
-	 * This is my initial suggestion for allocating refs to matches.
-	 * As I see it the GUI class will call this method when the allocate refs button is pressed.
-	 * First it uses toArray() to create a normal array from the ArrayList - this is because I want to use
-	 * Arrays.sort() to sort the list based on number of allocations (see compareTo in Referee class) and 
-	 * it needs to be an array for that to work. This array gets sorted.
-	 * Then it creates three separate ArrayLists for local refs, adjacent refs and non-adjacent refs.
-	 * Then it loops through the sorted array, picking out suitable refs and placing them in
-	 * the appropriate ArrayList.
-	 * At the end we have three ArrayLists sorted on number of allocations. These are combined into
-	 * 1 ArrayList sorted in order of suitability for the match, which is returned to the GUI class.
-	 * 
-	 * Fairly long method but quite hard to split up. In any case this is just the starting point
-	 * I guess.
-	 */
-	
+		
 	public void printReferees ()
 	{
 		for (Referee r : refList)
@@ -145,33 +147,46 @@ public class RefList implements Iterable<Referee> {
 		}
 	}
 
+	/**
+	 * takes in information about a match and creates a list of suitable refs, in order of suitability.
+	 * makes an array of Referees from refList then sorts that array in order of number of allocations.
+	 * then loops through the sorted array, checking each ref's suitability for the match based on their
+	 * home area and willingness to travel and placing them in the appropriate position in a new List:
+	 * first the local refs, then the adjacent refs, then the non-adjacent refs
+	 * @param matchLoc the location of the match
+	 * @param seniorMatch boolean indicating whether match is senior or not
+	 * @return List populated with suitable refs in order of suitability for the match
+	 */
+
 	public List<Referee> getSuitableRefs(int matchLoc, boolean seniorMatch) {
-		Referee[] arrayToSort = new Referee[refList.size()];
-		arrayToSort = refList.toArray(arrayToSort);
-		Arrays.sort(arrayToSort);
+		//make an array of Referees from refList
+		Referee[] arrayToSort = new Referee[refList.size()]; 
+		arrayToSort = refList.toArray(arrayToSort); 
+		Arrays.sort(arrayToSort); //sort the array on number of allocations
 
-		//array indecees
-		int localIndex = 0;
-		int adjIndex = 0;
-		int nonAdjIndex = 0;
-
+		//count of local and adjacent refs already in suitable refs list
+		int numLocalRefs = 0;
+		int numAdjRefs = 0;
+		//arrayList to be populated with suitable referees
 		List<Referee> suitableRefs = new ArrayList<Referee>();
 
-		for(int i=0; i < arrayToSort.length; i++) {
+		for(int i=0; i < arrayToSort.length; i++) { //loop through array sorted on number of allocations
 			Referee ref = arrayToSort[i];
-			int home = ref.getHomeArea();
-			boolean refWillTravel = ref.getTravelInfo(matchLoc);
+			int home = ref.getHomeArea(); //get ref's home area
+			boolean refWillTravel = ref.getTravelInfo(matchLoc); //get whether ref will travel to match location
 
-			if(!seniorMatch || ref.checkIfQualified()) {
-				if (home == matchLoc) {
-					suitableRefs.add(localIndex, ref);
-					localIndex++;
+			if(!seniorMatch || ref.checkIfQualified()) { //first check if ref is qualified for the match
+				//if ref lives in match location, add ref in position after last local ref
+				if (home == matchLoc) { 
+					suitableRefs.add(numLocalRefs, ref);
+					numLocalRefs++; //increment number of local refs in list
+				//if ref lives in an adjacent area, add ref in position after last adjacent ref
 				} else if ((home == Referee.CENTRAL || matchLoc == Referee.CENTRAL) && refWillTravel) {
-					suitableRefs.add(localIndex + adjIndex, ref);
-					adjIndex++;
+					suitableRefs.add(numLocalRefs + numAdjRefs, ref);
+					numAdjRefs++; //increment number of adjacent refs in list
+				//if ref lives in a non-adjacent area, add ref to end of list
 				} else if (refWillTravel) {
-					suitableRefs.add(localIndex + adjIndex + nonAdjIndex, ref);
-					nonAdjIndex++;
+					suitableRefs.add(ref);
 				}
 			}
 		}
@@ -179,7 +194,6 @@ public class RefList implements Iterable<Referee> {
 	}
 	/**
 	 * Returns an iterator over elements of type Referee.
-	 *
 	 * @return an Iterator.
 	 */
 	public Iterator<Referee> iterator() {
