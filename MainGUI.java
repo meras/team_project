94a556a0e02f13b1c5a9e5cab7bfc6baf5506eb6
tvz <tvzeller@gmail.com>
2015-02-08 10:53:20 +0000

    and one more tiny change

--- MainGUI.java
+++ MainGUI.java
@@ -417,7 +417,7 @@ public class MainGUI extends JFrame implements ActionListener {
 	{
 		tabbedPane.setSelectedIndex(1);
 
-		String allocated = "The referees allocated to the match are \n"+suitableRefs.get(0).getFName()+" "+suitableRefs.get(0).getLName()+" and "+suitableRefs.get(1).getFName()+" "+suitableRefs.get(1).getLName()
+		String allocated = "The referees allocated to the match are: \n"+suitableRefs.get(0).getFName()+" "+suitableRefs.get(0).getLName()+" and "+suitableRefs.get(1).getFName()+" "+suitableRefs.get(1).getLName()
 				+"\n\nThe referees which are suitable for the match are: \n";
 		StringBuilder display = new StringBuilder(allocated);
 
